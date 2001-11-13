5eaeb1d7010e7692263cffc549f295e13e8e3dbf
Thomas Krennwallner <tk@postsubmeta.net>
2001-11-13 14:48:46 +0000

    update AC_PREREQ and AUTOMAKE_OPTIONS

--- autogen.sh
+++ autogen.sh
@@ -2,10 +2,12 @@
 # do the funky auto* stuff
 
 echo "Generating configure script using autoconf and automake"
+set +x
 aclocal
 autoconf
 autoheader
 rm -rf intl
 gettextize --force
 automake --ignore-deps --add-missing --gnu
+set -x
 echo "Now you are ready to run ./configure with the desired options"
