8ee78978c8edebf5563ae2f25af81be42c75f2a8
Thomas Krennwallner <tk@postsubmeta.net>
2003-08-13 20:18:57 +0000

    Include with guards all possible ether_hostton header files.

--- lib/ether.h
+++ lib/ether.h
@@ -9,6 +9,30 @@
 #include <netinet/ether.h>
 #endif /* HAVE_NETINET_ETHER_H */
 
+#ifdef HAVE_NET_ETHERNET_H
+#include <net/ethernet.h>
+#endif /* HAVE_NET_ETHERNET_H */
+
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif /* HAVE_SYS_TYPES_H */
+
+#ifdef HAVE_SYS_SOCKET_H
+#include <sys/socket.h>
+#endif /* HAVE_SYS_SOCKET_H */
+
+#ifdef HAVE_NET_IF_H
+#include <net/if.h>
+#endif /* HAVE_NET_IF_H */
+
+#ifdef HAVE_NETINET_IN_H
+#include <netinet/in.h>
+#endif /* HAVE_NETINET_IN_H */
+
+#ifdef HAVE_NETINET_IF_ETHER_H
+#include <netinet/if_ether.h>
+#endif /* HAVE_NETINET_IF_ETHER_H */
+
 #include "magic.h"
 
 #if !HAVE_STRUCT_ETHER_ADDR_ETHER_ADDR_OCTET && !HAVE_STRUCT_ETHER_ADDR_OCTET
