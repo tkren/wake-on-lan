f06277d1ae5b6810de16bd570126e9f6aa6a3923
Thomas Krennwallner <tk@postsubmeta.net>
2004-02-05 18:14:19 +0000

    Add preliminary non-functional proxy support.

--- src/wol.c
+++ src/wol.c
@@ -1,26 +1,26 @@
 /*
- *	wol - wake on lan client
+ * wol - wake on lan client
  *
- *	main program
+ * main program
  * 
- *	$Id: wol.c,v 1.15 2003/08/09 19:26:45 wol Exp $
+ * $Id: wol.c,v 1.16 2003/08/10 08:18:38 wol Exp $
  *
- *	Copyright (C) 2000-2003 Thomas Krennwallner <krennwallner@aon.at>
+ * Copyright (C) 2000,2001,2002,2003,2004 Thomas Krennwallner <krennwallner@aon.at>
  *
- *	This program is free software; you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation; either version 2 of the License, or
- *	(at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *	You should have received a copy of the GNU General Public License
- *	along with this program; if not, write to the Free Software
- *	Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
- *	USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+ * USA.
  */
 
 
@@ -71,6 +71,9 @@ static int request_stdin = 0;
 /* be verbose */
 static int verbose = 0;
 
+/* send proxy packet */
+static int proxy_mode = 0;
+
 /* how long to wait between packets */
 static int msecs = 0;
 
@@ -85,14 +88,13 @@ static int sockfd = -1;
 static void
 usage (int status)
 {
-	if (status)
-		{
-			fprintf (stderr, _("Try `%s --help' for more information.\n"),
-												program_name);
-		}
-	else
-		{
-			fprintf (stdout, _("\
+  if (status)
+    {
+      fprintf (stderr, _("Try `%s --help' for more information.\n"), program_name);
+    }
+  else
+    {
+      fprintf (stdout, _("\
 Usage: %s [OPTION] ... MAC-ADDRESS ...\n\
 Wake On LAN client - wakes up magic packet compliant machines.\n\n\
     --help          display this help and exit\n\
@@ -110,14 +112,14 @@ Each MAC-ADDRESS is written as x:x:x:x:x:x, where x is a hexadecimal number\n\
 between 0 and ff which represents one byte of the address, which is in\n\
 network byte order (big endian).\n"), program_name);
 
-			fprintf (stdout, _("\n\
+      fprintf (stdout, _("\n\
 PASS is written as x-x-x-x-x-x, where x is a hexadecimal number between 0\n\
 and ff which represents one byte of the password.\n"));
 
-			fprintf (stdout, _("\nReport bugs to <krennwallner@aon.at>\n"));
-		}
+      fprintf (stdout, _("\nReport bugs to <krennwallner@aon.at>\n"));
+    }
 
-	exit (status);
+  exit (status);
 }
 
 
@@ -125,9 +127,9 @@ and ff which represents one byte of the password.\n"));
 static void
 version (void)
 {
-	fprintf (stdout, PACKAGE " " VERSION "\n\n");
-	fprintf (stdout, _("\
-Copyright (C) 2000-2003 Thomas Krennwallner <krennwallner@aon.at>\n\
+  fprintf (stdout, PACKAGE " " VERSION "\n\n");
+  fprintf (stdout, _("\
+Copyright (C) 2000-2004 Thomas Krennwallner <krennwallner@aon.at>\n\
 This is free software; see the source for copying conditions. There is NO\n\
 warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\
 \n"));
@@ -139,178 +141,194 @@ warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\
 static int
 parse_args (int argc, char *argv[])
 {
-	int c;
-	int option_index;
-	int password_set = 0;
-	char *options = "Vvw:h:i:p:f:-";
-	static struct option long_options[] = 
-		{
-			{ "help", no_argument, NULL, 'H' },
-			{ "version", no_argument, NULL, 'V' },
-			{ "verbose", no_argument, NULL, 'v' },
-			{ "wait", required_argument, NULL, 'w' },
-			{ "host", required_argument, NULL, 'h' },
-			{ "ipaddr", required_argument, NULL, 'i' },
-			{ "port", required_argument, NULL, 'p' },
-			{ "file", required_argument, NULL, 'f' },
-			{ "passwd", optional_argument, NULL, 'P' },
-			{ NULL, 0, NULL, 0 }
-		};
+  int c;
+  int option_index;
+  int password_set = 0;
+  char *options = "Vvw:h:i:p:f:s:-";
+  static struct option long_options[] = 
+    {
+      { "help", no_argument, NULL, 'H' },
+      { "version", no_argument, NULL, 'V' },
+      { "verbose", no_argument, NULL, 'v' },
+      { "wait", required_argument, NULL, 'w' },
+      { "host", required_argument, NULL, 'h' },
+      { "ipaddr", required_argument, NULL, 'i' },
+      { "port", required_argument, NULL, 'p' },
+      { "file", required_argument, NULL, 'f' },
+      { "passwd", optional_argument, NULL, 'P' },
+      { "proxy", required_argument, NULL, 's' },
+      { NULL, 0, NULL, 0 }
+    };
 
 
-	if (argc == 1)
+  if (argc == 1)
+    {
+      error (0, 0, _("Too few arguments."));
+      usage (1);
+    }
+
+
+
+  for (;;)
+    {
+      c = getopt_long (argc, argv, options, long_options, &option_index);
+      if (c == -1) break;
+
+      switch (c)
+	{
+	case 'H':
+	  usage (0);
+	  break;
+
+	  
+	case 'V':
+	  version ();
+	  exit (0);
+	  break;
+
+	  
+	case 'v':
+	  verbose = 1;
+	  break;
+
+
+	case 'w':
+	  if (sscanf (optarg, "%u", &msecs) != 1)
+	    {
+	      error (0, 0, _("Invalid time given"));
+	      usage (1);
+	    }
+	  msecs *= 1000;
+	  break;
+
+
+	case 's':
+	  proxy_mode = 1;
+	case 'h':
+	case 'i':
+	  host_str = optarg;
+	  break;
+
+
+	case 'p':
+	  if ((sscanf (optarg, "%5u", &port) != 1) ||
+	      port > 65535 || port == 0)
+	    {
+	      error (0, 0, _("Invalid port given"));
+	      usage (1);
+	    }
+	  break; 
+
+
+	case 'f':
+	  pathname = optarg;
+	  break;
+
+
+	case 'P':
+	  if (optarg == NULL)
+	    {
+	      size_t n;
+	      
+	      if (password_set)
+		break;
+	      
+	      if (getpass4 (_("Password"), &passwd, &n, stdin) == -1)
 		{
-			error (0, 0, _("Too few arguments."));
-			usage (1);
+		  error (1, 0, "getpass4 failed");
 		}
+	      password_set = 1;
+	    }
+	  else
+	    {
+	      passwd = optarg;
+	    }
+	  break;
 
 
+	case '?':
+	  break;
+	}
+    }
 
-	for (;;)
-		{
-			c = getopt_long (argc, argv, options, long_options, &option_index);
-			if (c == -1) break;
+  if ((optind == argc) && (pathname == NULL))
+    {
+      error (0, 0, _("You must specify at least one MAC-ADDRESS."));
+      usage (1);
+    }
 
-			switch (c)
-				{
-					case 'H':
-						usage (0);
-						break;
+  /* check if stdin is requested */
+  if (optind < argc)
+    {
+      int i;
 
+      for (i = optind; i < argc; ++i)
+	{
+	  if (argv[i][0] == '-' && argv[i][1] == 0)
+	    {
+	      request_stdin = 1;
+	      break;
+	    }
+	}
+    }
+  else if (pathname != NULL)
+    {
+      if (!strncmp (pathname, "-", 1))
+	{
+	  request_stdin = 1;
+	}
+    }
 
-					case 'V':
-						version ();
-						exit (0);
-						break;
-
-
-					case 'v':
-						verbose = 1;
-						break;
-
-
-					case 'w':
-						if (sscanf (optarg, "%u", &msecs) != 1)
-							{
-								error (0, 0, _("Invalid time given"));
-								usage (1);
-							}
-						msecs *= 1000;
-						break;
-
-
-					case 'h':
-					case 'i':
-						host_str = optarg;
-						break;
-
-
-					case 'p':
-						if ((sscanf (optarg, "%5u", &port) != 1)
-								|| port > 65535 || port == 0)
-							{
-								error (0, 0, _("Invalid port given"));
-								usage (1);
-							}
-						break; 
-
-
-					case 'f':
-						pathname = optarg;
-						break;
-
-
-					case 'P':
-						if (optarg == NULL)
-							{
-								size_t n;
-
-								if (password_set)
-									break;
-
-								if (getpass4 (_("Password"), &passwd, &n, stdin) == -1)
-									{
-										error (1, 0, "getpass4 failed");
-									}
-								password_set = 1;
-							}
-						else
-							passwd = optarg;
-						break;
-
-
-					case '?':
-						break;
-				}
-		}
-
-	if ((optind == argc) && (pathname == NULL))
-		{
-			error (0, 0, _("You must specify at least one MAC-ADDRESS."));
-			usage (1);
-		}
-
-	/* check if stdin is requested */
-	if (optind < argc)
-		{
-			int i;
-
-			for (i = optind; i < argc; ++i)
-				{
-					if (argv[i][0] == '-' && argv[i][1] == 0)
-						{
-							request_stdin = 1;
-							break;
-						}
-				}
-		}
-	else if (pathname != NULL)
-		{
-			if (!strncmp (pathname, "-", 1)) request_stdin = 1;
-		}
-
-	/* return the offset of the GNU getopt sorted parameters */
-	return optind;
+  /* return the offset of the GNU getopt sorted parameters */
+  return optind;
 }
 
 
 
 static int
-assemble_and_send (struct magic *m, const char *mac_str, const char *host_str,
-										unsigned int portnum, const char *pass_str, int socketfd)
+assemble_and_send (struct magic *m,
+		   const char *mac_str,
+		   const char *host_str,
+		   unsigned int portnum,
+		   const char *pass_str,
+		   int socketfd)
 {
-	int ret = magic_assemble (m, mac_str, pass_str);
+  int ret = magic_assemble (m, mac_str, pass_str);
 	
-	switch (ret)
-		{
-			case -1:
-				error (0, errno, _("Cannot assemble magic packet for '%s'"), mac_str);
-				errno = 0;
-				return -1;
+  switch (ret)
+    {
+    case -1:
+      error (0, errno, _("Cannot assemble magic packet for '%s'"), mac_str);
+      errno = 0;
+      return -1;
 
-			case -2:
-				error (0, 0, _("Invalid password given for '%s'"), mac_str);
-				errno = 0;
-				return -1;
-		}
+    case -2:
+      error (0, 0, _("Invalid password given for '%s'"), mac_str);
+      errno = 0;
+      return -1;
+    }
 
-	if (net_send (socketfd, host_str, portnum, m->packet, m->size))
-		{
-			error (0, errno, _("Cannot send magic packet for '%s' to %s:%d"),
-												mac_str, host_str, portnum);
-			errno = 0;
-			return -1;
-		}
+  if (udp_send (socketfd, host_str, portnum, m->packet, m->size))
+    {
+      error (0, errno, _("Cannot send magic packet for '%s' to %s:%d"),
+	     mac_str, host_str, portnum);
+      errno = 0;
+      return -1;
+    }
 
-	fprintf (stdout, _("Waking up %s"), mac_str);
-	if (verbose)
-		fprintf (stdout, _(" with %s:%d"), host_str, portnum);
-	fprintf (stdout, _("...\n"));
+  fprintf (stdout, _("Waking up %s"), mac_str);
+  if (verbose)
+    {
+      fprintf (stdout, _(" with %s:%d"), host_str, portnum);
+    }
+  fprintf (stdout, _("...\n"));
 
-	if (msecs)
-		usleep (msecs);
+  if (msecs)
+    {
+      usleep (msecs);
+    }
 
-	return 0;
+  return 0;
 }
 
 
@@ -318,76 +336,106 @@ assemble_and_send (struct magic *m, const char *mac_str, const char *host_str,
 int 
 main (int argc, char *argv[])
 {
-	int i;
-	int ret = 0;
+  int i;
+  int ret = 0;
 
-	/* my name is ... */
-	program_name = argv[0];
+  /* my name is ... */
+  program_name = argv[0];
 
 #if ENABLE_NLS
-	setlocale (LC_ALL, "");
-	bindtextdomain (PACKAGE, LOCALEDIR);
-	textdomain (PACKAGE);
+  setlocale (LC_ALL, "");
+  bindtextdomain (PACKAGE, LOCALEDIR);
+  textdomain (PACKAGE);
 #endif /* ENABLE_NLS */
 	
-	i = parse_args (argc, argv);
+  i = parse_args (argc, argv);
 
-	magic = magic_create (passwd != NULL);
-	if (magic == NULL) exit (1);
+  magic = magic_create (passwd != NULL);
+  if (magic == NULL)
+    {
+      exit (1);
+    }
 
-	sockfd = net_open ();
-	if (sockfd < 0) exit (1);
+  if (!proxy_mode)
+    {
+      sockfd = udp_open ();
+    }
+  else
+    {
+      sockfd = tcp_open (host_str, port);
+    }
+
+  if (sockfd < 0)
+    {
+      exit (1);
+    }
 
 
-	/* loop through possible MAC addresses */
-	if (!request_stdin)
+  /* loop through possible MAC addresses */
+  if (!request_stdin)
+    {
+      if (!proxy_mode)
+	{
+	  for (; i < argc; i++)
+	    {
+	      ret -= assemble_and_send (magic, argv[i], host_str, port, passwd,	sockfd);
+	    }
+	}
+      else
+	{
+	  /* FIXME: proxy mode */
+	}
+    }
+
+
+  /* -f given */
+  if (pathname || request_stdin)
+    {
+      FILE *fp;
+
+      if (request_stdin)
+	{
+	  fp = stdin;
+	}
+      else
+	{
+	  fp = fopen (pathname, "r");
+	  if (fp == NULL)
+	    {
+	      error (1, errno, "%s", pathname);
+	    }
+	}
+
+      if (!proxy_mode)
+	{
+	  /* loop through fp */
+	  for (;;)
+	    {
+	      if (macfile_parse (fp, &mac_str, &host_str, &port, &passwd)) break;
+	      
+	      if (port == 0 || port > 65535)
 		{
-			for (; i < argc; i++)
-				{
-					ret -= assemble_and_send (magic, argv[i], host_str, port, passwd,
-																		sockfd);
-				}
+		  port = DEFAULT_PORT;
 		}
 
+	      ret -= assemble_and_send (magic, mac_str, host_str, port, passwd,	sockfd);
+	  
+	      XFREE (mac_str);
+	      XFREE (host_str);
+	      XFREE (passwd);
+	    }
+	}
+      else
+	{
+	  /* FIXME: proxy mode */
+	}
 
-	/* -f given */
-	if (pathname || request_stdin)
-		{
-			FILE *fp;
+      fclose (fp);
+    }
 
-			if (request_stdin)
-				{
-					fp = stdin;
-				}
-			else
-				{
-					fp = fopen (pathname, "r");
-					if (fp == NULL)
-						error (1, errno, "%s", pathname);
-				}
+  net_close (sockfd);
 
-			/* loop through fp */
-			for (;;)
-				{
-					if (macfile_parse (fp, &mac_str, &host_str, &port, &passwd)) break;
-
-					if (port == 0 || port > 65535)
-						port = DEFAULT_PORT;
-
-					ret -= assemble_and_send (magic, mac_str, host_str, port, passwd,
-																		sockfd);
-
-					XFREE (mac_str);
-					XFREE (host_str);
-					XFREE (passwd);
-				}
-
-			fclose (fp);
-		}
-
-	net_close (sockfd);
-
-	magic_destroy (magic);
-
-	exit (ret != 0);
+  magic_destroy (magic);
+  
+  exit (ret != 0);
 }
